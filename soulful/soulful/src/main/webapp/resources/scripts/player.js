e08e3fb6814aa3f5e905ef0aeeb033ff5cbd7541
bigdata02 <bigdata02@DESKTOP-8K30UI9>
2021-11-02 14:06:48 +0900

    그래프, 사진 오류 수정

--- soulful/soulful/src/main/webapp/resources/scripts/player.js
+++ soulful/soulful/src/main/webapp/resources/scripts/player.js
@@ -47,7 +47,7 @@
             "except": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Nihil illinc huc pervenit.",
             "link": "track.detail.html",
             "thumb": { "src": "../resources/images/b1.jpg" },
-            "src": "http://api.soundcloud.com/tracks/269944843/stream?client_id=a10d44d431ad52868f1bce6d36f5234c",
+            "src": "resources/musics/Put Me Thru.mp3",
             "meta": {
                 "author": "Summerella",
                 "authorlink": "artist.detail.html",
